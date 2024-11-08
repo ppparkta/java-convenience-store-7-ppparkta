1a83a6abc2446ba41d9a01fcfd0b7618fbf22f97
ppparkta <pppppark2@gmail.com>
2024-11-08 19:18:45 +0900

    feat(PromotionType): 프로모션 타입 생성 로직 추가

---
+++ src/main/java/store/model/PromotionType.java
@@ -0,0 +1,55 @@
+package store.model;
+
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.time.LocalDate;
+import store.constant.ExceptionMessage;
+import store.constant.GeneralConfig;
+import store.exception.ExceptionUtils;
+
+public class PromotionType {
+    private final String name;
+    private final int buy;
+    private final int get;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    public PromotionType(String name, int buy, int get, LocalDate startDate, LocalDate endDate) {
+        validate(name, buy, get, startDate, endDate);
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    private void validate(String name, int buy, int get, LocalDate startDate, LocalDate endDate) {
+        if (name == null || name.isEmpty()) {
+            ExceptionUtils.throwIllegalArgumentException(ExceptionMessage.ERROR_EMPTY_INPUT);
+        }
+        validateQuantity(buy);
+        validateQuantity(get);
+        validateStartDate(startDate, endDate);
+        validateEndDate(endDate);
+    }
+
+    private void validateEndDate(LocalDate endDate) {
+        if (endDate.isBefore(DateTimes.now().toLocalDate())) {
+            ExceptionUtils.throwIllegalArgumentException(ExceptionMessage.INVALID_PROMOTION_END_DATE);
+        }
+    }
+
+    private void validateStartDate(LocalDate startDate, LocalDate endDate) {
+        if (startDate.isAfter(endDate)) {
+            ExceptionUtils.throwIllegalArgumentException(ExceptionMessage.INVALID_PROMOTION_START_DATE);
+        }
+    }
+
+    private void validateQuantity(int quantity) {
+        if (quantity < GeneralConfig.PROMOTION_MIN_QUANTITY.getValue()) {
+            ExceptionUtils.throwIllegalArgumentException(ExceptionMessage.INVALID_MIN_QUANTITY);
+        }
+        if (quantity > Integer.MAX_VALUE) {
+            ExceptionUtils.throwIllegalArgumentException(ExceptionMessage.INVALID_INTEGER_RANGE);
+        }
+    }
+}
