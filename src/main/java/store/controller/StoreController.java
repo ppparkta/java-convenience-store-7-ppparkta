2b4ef8705655a6fdbc61d5772793139141dae25a
ppparkta <pppppark2@gmail.com>
2024-11-11 01:43:17 +0900

    feat(OutputView): 재고 출력 기능 추가

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -4,10 +4,14 @@ import store.model.product.ProductManager;
 
 public class StoreController {
     public void run() {
-        ProductController productController = new ProductController();
-        ProductManager productManager = productController.initialize();
+        try {
+            ProductController productController = new ProductController();
+            ProductManager productManager = productController.initialize();
 
-        OrderController orderController = new OrderController(productManager);
-        orderController.processOrder();
+            OrderController orderController = new OrderController(productManager);
+            orderController.processOrder();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,43 @@
+package store.view;
+
+import java.util.List;
+import java.util.Optional;
+import store.model.PromotionType;
+import store.model.product.Stock;
+
+public class OutputView {
+    public void printWelcomeMessage() {
+        System.out.println("안녕하세요. W편의점입니다.");
+        System.out.println("현재 보유하고 있는 상품입니다.");
+        System.out.println();
+    }
+
+    public void printStocks(List<Stock> stocks) {
+        for (Stock stock : stocks) {
+            printStock(stock);
+        }
+    }
+
+    private void printStock(Stock stock) {
+        String productName = stock.getProduct().getName();
+        long price = stock.getProduct().getPrice();
+        int quantity = stock.getQuantity();
+        Optional<PromotionType> promotionType = stock.getProduct().getPromotionType();
+
+        printStockFormat(productName, price, quantity, promotionType);
+    }
+
+    private static void printStockFormat(String productName, long price, int quantity,
+                                         Optional<PromotionType> promotionType) {
+        String promotion = promotionType.map(PromotionType::getName).orElse("");
+
+        if (quantity == 0) {
+            System.out.println(
+                    String.format("- %s %,d원 재고 없음 %s", productName, price, promotion));
+        } else {
+            System.out.println(
+                    String.format("- %s %,d원 %,d개 %s", productName, price, quantity, promotion));
+        }
+    }
+}
+
